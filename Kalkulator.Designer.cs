812023dc02034c8d3b223cc0c5f05cc47c15b94a
alkiin01 <67543549+alkiin01@users.noreply.github.com>
2021-09-14 23:09:12 +0700

    Add files via upload

---
+++ Kalkulator.Designer.cs
@@ -0,0 +1,326 @@
+﻿
+namespace TestForm
+{
+    partial class Kalkulator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.angka1 = new System.Windows.Forms.Button();
+            this.angka2 = new System.Windows.Forms.Button();
+            this.angka3 = new System.Windows.Forms.Button();
+            this.angka4 = new System.Windows.Forms.Button();
+            this.angka5 = new System.Windows.Forms.Button();
+            this.angka6 = new System.Windows.Forms.Button();
+            this.angka7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.hasil = new System.Windows.Forms.Button();
+            this.kali = new System.Windows.Forms.Button();
+            this.tambah = new System.Windows.Forms.Button();
+            this.bagi = new System.Windows.Forms.Button();
+            this.kurang = new System.Windows.Forms.Button();
+            this.hapus = new System.Windows.Forms.Button();
+            this.numBox = new System.Windows.Forms.TextBox();
+            this.akar = new System.Windows.Forms.Button();
+            this.persen = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // angka1
+            // 
+            this.angka1.Location = new System.Drawing.Point(32, 95);
+            this.angka1.Name = "angka1";
+            this.angka1.Size = new System.Drawing.Size(40, 32);
+            this.angka1.TabIndex = 0;
+            this.angka1.Text = "1";
+            this.angka1.UseVisualStyleBackColor = true;
+            this.angka1.Click += new System.EventHandler(this.angka1_Click);
+            // 
+            // angka2
+            // 
+            this.angka2.Location = new System.Drawing.Point(78, 95);
+            this.angka2.Name = "angka2";
+            this.angka2.Size = new System.Drawing.Size(39, 32);
+            this.angka2.TabIndex = 1;
+            this.angka2.Text = "2";
+            this.angka2.UseVisualStyleBackColor = true;
+            this.angka2.Click += new System.EventHandler(this.angka2_Click);
+            // 
+            // angka3
+            // 
+            this.angka3.Location = new System.Drawing.Point(123, 95);
+            this.angka3.Name = "angka3";
+            this.angka3.Size = new System.Drawing.Size(39, 32);
+            this.angka3.TabIndex = 2;
+            this.angka3.Text = "3";
+            this.angka3.UseVisualStyleBackColor = true;
+            this.angka3.Click += new System.EventHandler(this.angka3_Click);
+            // 
+            // angka4
+            // 
+            this.angka4.Location = new System.Drawing.Point(33, 133);
+            this.angka4.Name = "angka4";
+            this.angka4.Size = new System.Drawing.Size(39, 32);
+            this.angka4.TabIndex = 3;
+            this.angka4.Text = "4";
+            this.angka4.UseVisualStyleBackColor = true;
+            this.angka4.Click += new System.EventHandler(this.angka4_Click);
+            // 
+            // angka5
+            // 
+            this.angka5.Location = new System.Drawing.Point(78, 133);
+            this.angka5.Name = "angka5";
+            this.angka5.Size = new System.Drawing.Size(39, 32);
+            this.angka5.TabIndex = 4;
+            this.angka5.Text = "5";
+            this.angka5.UseVisualStyleBackColor = true;
+            this.angka5.Click += new System.EventHandler(this.angka5_Click);
+            // 
+            // angka6
+            // 
+            this.angka6.Location = new System.Drawing.Point(123, 133);
+            this.angka6.Name = "angka6";
+            this.angka6.Size = new System.Drawing.Size(39, 32);
+            this.angka6.TabIndex = 5;
+            this.angka6.Text = "6";
+            this.angka6.UseVisualStyleBackColor = true;
+            this.angka6.Click += new System.EventHandler(this.angka6_Click);
+            // 
+            // angka7
+            // 
+            this.angka7.Location = new System.Drawing.Point(33, 171);
+            this.angka7.Name = "angka7";
+            this.angka7.Size = new System.Drawing.Size(39, 32);
+            this.angka7.TabIndex = 6;
+            this.angka7.Text = "7";
+            this.angka7.UseVisualStyleBackColor = true;
+            this.angka7.Click += new System.EventHandler(this.angka7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(78, 171);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(39, 32);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "8";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(123, 171);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(39, 32);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "9";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(123, 209);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(39, 32);
+            this.button8.TabIndex = 9;
+            this.button8.Text = ".";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(78, 209);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(39, 32);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "0";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // hasil
+            // 
+            this.hasil.Location = new System.Drawing.Point(218, 209);
+            this.hasil.Name = "hasil";
+            this.hasil.Size = new System.Drawing.Size(39, 32);
+            this.hasil.TabIndex = 11;
+            this.hasil.Text = "=";
+            this.hasil.UseVisualStyleBackColor = true;
+            this.hasil.Click += new System.EventHandler(this.hasil_Click);
+            // 
+            // kali
+            // 
+            this.kali.Location = new System.Drawing.Point(169, 95);
+            this.kali.Name = "kali";
+            this.kali.Size = new System.Drawing.Size(43, 32);
+            this.kali.TabIndex = 12;
+            this.kali.Text = "*";
+            this.kali.UseVisualStyleBackColor = true;
+            this.kali.Click += new System.EventHandler(this.kali_Click);
+            // 
+            // tambah
+            // 
+            this.tambah.Location = new System.Drawing.Point(169, 209);
+            this.tambah.Name = "tambah";
+            this.tambah.Size = new System.Drawing.Size(43, 32);
+            this.tambah.TabIndex = 13;
+            this.tambah.Text = "+";
+            this.tambah.UseVisualStyleBackColor = true;
+            this.tambah.Click += new System.EventHandler(this.tambah_Click);
+            // 
+            // bagi
+            // 
+            this.bagi.Location = new System.Drawing.Point(169, 133);
+            this.bagi.Name = "bagi";
+            this.bagi.Size = new System.Drawing.Size(43, 32);
+            this.bagi.TabIndex = 14;
+            this.bagi.Text = "/";
+            this.bagi.UseVisualStyleBackColor = true;
+            this.bagi.Click += new System.EventHandler(this.bagi_Click);
+            // 
+            // kurang
+            // 
+            this.kurang.Location = new System.Drawing.Point(169, 171);
+            this.kurang.Name = "kurang";
+            this.kurang.Size = new System.Drawing.Size(43, 32);
+            this.kurang.TabIndex = 15;
+            this.kurang.Text = "-";
+            this.kurang.UseVisualStyleBackColor = true;
+            this.kurang.Click += new System.EventHandler(this.kurang_Click);
+            // 
+            // hapus
+            // 
+            this.hapus.Location = new System.Drawing.Point(32, 209);
+            this.hapus.Name = "hapus";
+            this.hapus.Size = new System.Drawing.Size(40, 32);
+            this.hapus.TabIndex = 16;
+            this.hapus.Text = "C";
+            this.hapus.UseVisualStyleBackColor = true;
+            this.hapus.Click += new System.EventHandler(this.hapus_Click);
+            // 
+            // numBox
+            // 
+            this.numBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numBox.Location = new System.Drawing.Point(33, 35);
+            this.numBox.Multiline = true;
+            this.numBox.Name = "numBox";
+            this.numBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.numBox.Size = new System.Drawing.Size(224, 54);
+            this.numBox.TabIndex = 17;
+            // 
+            // akar
+            // 
+            this.akar.Location = new System.Drawing.Point(219, 133);
+            this.akar.Name = "akar";
+            this.akar.Size = new System.Drawing.Size(38, 31);
+            this.akar.TabIndex = 18;
+            this.akar.Text = "√";
+            this.akar.UseVisualStyleBackColor = true;
+            this.akar.Click += new System.EventHandler(this.akar_Click);
+            // 
+            // persen
+            // 
+            this.persen.Location = new System.Drawing.Point(218, 171);
+            this.persen.Name = "persen";
+            this.persen.Size = new System.Drawing.Size(39, 32);
+            this.persen.TabIndex = 19;
+            this.persen.Text = "%";
+            this.persen.UseVisualStyleBackColor = true;
+            this.persen.Click += new System.EventHandler(this.persen_Click);
+            // 
+            // delete
+            // 
+            this.delete.Location = new System.Drawing.Point(218, 95);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(39, 32);
+            this.delete.TabIndex = 20;
+            this.delete.Text = "del";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // Kalkulator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(299, 297);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.persen);
+            this.Controls.Add(this.akar);
+            this.Controls.Add(this.numBox);
+            this.Controls.Add(this.hapus);
+            this.Controls.Add(this.kurang);
+            this.Controls.Add(this.bagi);
+            this.Controls.Add(this.tambah);
+            this.Controls.Add(this.kali);
+            this.Controls.Add(this.hasil);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.angka7);
+            this.Controls.Add(this.angka6);
+            this.Controls.Add(this.angka5);
+            this.Controls.Add(this.angka4);
+            this.Controls.Add(this.angka3);
+            this.Controls.Add(this.angka2);
+            this.Controls.Add(this.angka1);
+            this.Name = "Kalkulator";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Kalkulator";
+            this.Load += new System.EventHandler(this.Kalkulator_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button angka1;
+        private System.Windows.Forms.Button angka2;
+        private System.Windows.Forms.Button angka3;
+        private System.Windows.Forms.Button angka4;
+        private System.Windows.Forms.Button angka5;
+        private System.Windows.Forms.Button angka6;
+        private System.Windows.Forms.Button angka7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button hasil;
+        private System.Windows.Forms.Button kali;
+        private System.Windows.Forms.Button tambah;
+        private System.Windows.Forms.Button bagi;
+        private System.Windows.Forms.Button kurang;
+        private System.Windows.Forms.Button hapus;
+        private System.Windows.Forms.TextBox numBox;
+        private System.Windows.Forms.Button akar;
+        private System.Windows.Forms.Button persen;
+        private System.Windows.Forms.Button delete;
+    }
+}
+
---
+++ Kalkulator.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestForm
+{
+    
+    public partial class Kalkulator : Form
+    {
+        double nilai = 0;
+        string ops = "";
+        bool op_pressed = false;
+        public Kalkulator()
+        {
+            InitializeComponent();
+        }
+
+        private void Kalkulator_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void angka1_Click(object sender, EventArgs e)
+        {
+            if (numBox.Text == "0")  { 
+                numBox.Clear();
+            }
+            Button b = (Button)sender;
+            numBox.Text = numBox.Text + b.Text;
+        }
+
+        private void angka2_Click(object sender, EventArgs e)
+        {
+            if (numBox.Text == "0")
+            {
+                numBox.Clear();
+            }
+            Button b = (Button)sender;
+            numBox.Text = numBox.Text + b.Text;
+        }
+
+        private void angka3_Click(object sender, EventArgs e)
+        {
+            if (numBox.Text == "0")
+            {
+                numBox.Clear();
+            }
+            Button b = (Button)sender;
+            numBox.Text = numBox.Text + b.Text;
+        }
+
+        private void angka4_Click(object sender, EventArgs e)
+        {
+            if (numBox.Text == "0") 
+            {
+                numBox.Clear();
+            }
+            Button b = (Button)sender;
+            numBox.Text = numBox.Text + b.Text;
+        }
+
+        private void angka5_Click(object sender, EventArgs e)
+        {
+            if (numBox.Text == "0")
+            {
+                numBox.Clear();
+            }
+            Button b = (Button)sender;
+            numBox.Text = numBox.Text + b.Text;
+        }
+
+        private void angka6_Click(object sender, EventArgs e)
+        {
+            if (numBox.Text == "0")
+            {
+                numBox.Clear();
+            }
+            Button b = (Button)sender;
+            numBox.Text = numBox.Text + b.Text;
+        }
+
+        private void angka7_Click(object sender, EventArgs e)
+        {
+            if (numBox.Text == "0")
+            {
+                numBox.Clear();
+            }
+            Button b = (Button)sender;
+            numBox.Text = numBox.Text + b.Text;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (numBox.Text == "0")
+            {
+                numBox.Clear();
+            }
+            Button b = (Button)sender;
+            numBox.Text = numBox.Text + b.Text;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (numBox.Text == "0")
+            {
+                numBox.Clear();
+            }
+            Button b = (Button)sender;
+            numBox.Text = numBox.Text + b.Text;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (numBox.Text == "0")
+            {
+                numBox.Clear();
+            }
+            Button b = (Button)sender;
+            numBox.Text = numBox.Text + b.Text;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+           
+            Button b = (Button)sender;
+            numBox.Text = numBox.Text + b.Text;
+        }
+
+        private void hapus_Click(object sender, EventArgs e)
+        {
+            numBox.Clear();
+            op_pressed = false;
+        }
+
+        private void kali_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            ops = b.Text;
+            nilai = Double.Parse(numBox.Text);
+            numBox.Clear();
+            op_pressed = true;
+        }
+
+        private void hasil_Click(object sender, EventArgs e)
+        {
+            switch (ops){
+                case "*" : numBox.Text = (nilai * Double.Parse(numBox.Text)).ToString();
+                    break;
+                case "/" : numBox.Text = (nilai / Double.Parse(numBox.Text)).ToString();
+                    break;
+                case "-" : numBox.Text = (nilai - Double.Parse(numBox.Text)).ToString();
+                    break;
+                case "+" : numBox.Text = (nilai + Double.Parse(numBox.Text)).ToString();
+                    break;
+            }
+        }
+
+        private void bagi_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            ops = b.Text;
+            nilai = Double.Parse(numBox.Text);
+            numBox.Clear();
+
+            op_pressed = true;
+        }
+
+        private void kurang_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            ops = b.Text;
+            nilai = Double.Parse(numBox.Text);
+            numBox.Clear();
+
+            op_pressed = true;
+        }
+
+        private void tambah_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            ops = b.Text;
+            nilai = Double.Parse(numBox.Text);
+            numBox.Clear();
+            op_pressed = true;
+        }
+
+        private void persen_Click(object sender, EventArgs e)
+        {
+            numBox.Text = (Double.Parse(numBox.Text) * 0.01).ToString();
+        }
+
+        private void akar_Click(object sender, EventArgs e)
+        {
+            numBox.Text = (Math.Sqrt(Double.Parse(numBox.Text))).ToString();
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            numBox.Text = numBox.Text.Remove(numBox.Text.Length - 1,1);
+        }
+    }
+
+}
